ef5c14a2fbe28536a51937364d28c181df4fab73
Viviane Bonadia <vivianebonadia@gmail.com>
2016-04-26 22:38:38 -0300

    Update sizeof.c

--- sizeof.c
+++ sizeof.c
@@ -2,7 +2,7 @@
 
 void main() {
 	printf("char:\t\t %ld bytes\n", sizeof(char));
-    printf("int :\t\t %ld bytes\n", sizeof(int));
-    printf("short int:\t %ld bytes\n", sizeof(short));
-    printf("long int:\t %ld bytes\n", sizeof(long ));
+	printf("int :\t\t %ld bytes\n", sizeof(int));
+	printf("short int:\t %ld bytes\n", sizeof(short));
+	printf("long int:\t %ld bytes\n", sizeof(long ));
 }
