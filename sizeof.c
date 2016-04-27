2c76978606590d16ae99f7e23d6445450f597798
Viviane Bonadia <vivianebonadia@gmail.com>
2016-04-26 22:37:50 -0300

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main() {
+	printf("char:\t\t %ld bytes\n", sizeof(char));
+    printf("int :\t\t %ld bytes\n", sizeof(int));
+    printf("short int:\t %ld bytes\n", sizeof(short));
+    printf("long int:\t %ld bytes\n", sizeof(long ));
+}
